9d2a8ce8878246c78ef8b3d24e742cc36950e233
YanqiangWang <qingtianguzhu@gmail.com>
2016-04-04 21:01:02 +0800

    update

--- 201512-3/Main.java
+++ 201512-3/Main.java
@@ -1,84 +1,103 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Main {
 
-	private int m, n, q;
-	private char[][] num;
+	private static int width, height, q;
+	private static char[][] num;
+	private static ArrayDeque<Element> queue;
 
 	public static void main(String[] args) {
-		Main m = new Main();
-		m.read_data();
-		m.print();
+		read_data();
+		print();
 	}
 
-	private void print() {
-		for (int i = n - 1; i >= 0; i--) {
-			for (int j = 0; j < m; j++) {
+	public static void print() {
+		for (int i = height - 1; i >= 0; i--) {
+			for (int j = 0; j < width; j++) {
 				System.out.print(num[i][j]);
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 
-	private void fill(int x, int y, char c) {
-		num[y][x] = c;
-		if (x - 1 >= 0 && flag(num[y][x - 1]) && num[y][x - 1] != c) {
-			fill(x - 1, y, c);
+	public static void fill(int w, int h, char temp) {
+		char c = '*';
+		if(temp == c)
+			c = '&';
+		Element elem = new Element(w, h);
+		queue = new ArrayDeque<Element>();
+		queue.addLast(elem);
+		while(!queue.isEmpty()) {
+			elem = queue.removeFirst();
+			int y = elem.w;
+			int x = elem.h;
+			num[x][y] = c;
+			if (0 <= x - 1 && flag(num[x - 1][y]) && num[x - 1][y] != c) {
+				queue.addLast(new Element(y, x - 1));
+			}
+			if (0 <= y - 1 && flag(num[x][y - 1]) && num[x][y - 1] != c) {
+				queue.addLast(new Element(y - 1, x));
+			}
+			if (x + 1 < height && flag(num[x + 1][y]) && num[x + 1][y] != c) {
+				queue.addLast(new Element(y, x + 1));
+			}
+			if (y + 1 < width && flag(num[x][y + 1]) && num[x][y + 1] != c) {
+				queue.addLast(new Element(y + 1, x));
+			}
 		}
-		if (x + 1 < m && flag(num[y][x + 1]) && num[y][x + 1] != c) {
-			fill(x + 1, y, c);
-		}
-		if (y - 1 >= 0 && flag(num[y - 1][x]) && num[y - 1][x] != c) {
-			fill(x, y - 1, c);
-		}
-		if (y + 1 < n && flag(num[y + 1][x]) && num[y + 1][x] != c) {
-			fill(x, y + 1, c);
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (num[i][j] == c)
+					num[i][j] = temp;
+			}
 		}
 	}
 
-	private boolean flag(char c) {
+	public static boolean flag(char c) {
 		return c != '|' && c != '-' && c != '+';
 	}
 
-	private void draw(int x1, int y1, int x2, int y2) {
-		if (x2 < x1) {
-			int temp = x1;
-			x1 = x2;
-			x2 = temp;
+	public static void draw(int w1, int h1, int w2, int h2) {
+		if (w2 < w1) {
+			int temp = w1;
+			w1 = w2;
+			w2 = temp;
 		}
-		if (y2 < y1) {
-			int temp = y1;
-			y1 = y2;
-			y2 = temp;
+		if (h2 < h1) {
+			int temp = h1;
+			h1 = h2;
+			h2 = temp;
 		}
-		if (x1 == x2) {
-			for (int i = y1; i <= y2; i++) {
-				if (num[i][x1] != '-') {
-					num[i][x1] = '|';
+		if (w1 == w2) {
+			for (int i = h1; i <= h2; i++) {
+				if (num[i][w1] != '-') {
+					num[i][w1] = '|';
 				} else {
-					num[i][x1] = '+';
+					num[i][w1] = '+';
 				}
 			}
 		}
-		if (y1 == y2) {
-			for (int i = x1; i <= x2; i++) {
-				if (num[y1][i] != '|') {
-					num[y1][i] = '-';
+		if (h1 == h2) {
+			for (int i = w1; i <= w2; i++) {
+				if (num[h1][i] != '|') {
+					num[h1][i] = '-';
 				} else {
-					num[y1][i] = '+';
+					num[h1][i] = '+';
 				}
 			}
 		}
 	}
 
-	private void read_data() {
+	public static void read_data() {
 		Scanner sc = new Scanner(System.in);
-		m = sc.nextInt();
-		n = sc.nextInt();
+		width = sc.nextInt();
+		height = sc.nextInt();
 		q = sc.nextInt();
-		num = new char[n][m];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+		num = new char[height][width];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
 				num[i][j] = '.';
 			}
 		}
@@ -92,4 +111,13 @@ public class Main {
 		sc.close();
 	}
 
+}
+
+class Element {
+	public int w, h;
+	
+	Element(int w, int h) {
+		this.w = w;
+		this.h = h;
+	}
 }
